70576d29f403dd4498bdb05050d3982505e43a71
MARTINCHUNGA <Bsc-77-16@cc.ac.mw>
2022-02-03 18:59:11 -0800

    forms and tables

---
+++ src/components/Forms/combineForms.js
@@ -0,0 +1,87 @@
+
+
+import React from 'react'
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import Radio from '@material-ui/core/Radio';
+import * as Yup from 'yup'
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+const Signup = () => {
+    const paperStyle = { padding: 20, width: 300, margin: "0 auto" }
+    const headerStyle = { margin: 0 }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const marginTop = { marginTop: 5 }
+    
+    // const initialValues = {
+    //     username: '',
+    //     email : '',
+    //     gender : '',
+    //     phoneNumber: '',
+    //     password: '',
+    //     confirmPassword: ''
+        
+    // }
+    // const validationSchema = Yup.object().shape({
+        
+    //         // firstName : string().required("Please enter you first name"),
+    //         // lastName : string().required("Please enter your last name"),
+    //         username : string().required("Please enter username").min(8, "username too short"),
+    //         email : string().required("Please enter email").email("Invalid email"),
+    //         gender : string().required("Enter your gender"),
+    //         password : string().required("Enter password").min(8,"Password too short"),
+    //         confirmPassword : string().required("Confirm your password").min(8,"Password too short")
+
+
+    //     // username: Yup.string().email('please enter valid email').required("Required"),
+    //     // email: Yup.string().email('please enter valid email').required("Required"),
+    //     // gender: Yup.string().email('please enter valid email').required("Required"),
+    //     // password: Yup.string().required("Required").min(8,"Password should contain atleast 8 characters")
+    // })
+    // const onSubmit = (values, props) => {
+    //     console.log(values)
+    //     setTimeout(() => {
+    //         props.resetForm()
+    //         props.setSubmitting(false)
+    //     }, 2000)
+
+    // }
+
+    return (
+        <Grid>
+            <Paper style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}>
+                        <AddCircleOutlineOutlinedIcon />
+                    </Avatar>
+                    <h2 style={headerStyle}>Sign Up</h2>
+                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+                </Grid>
+                <form>
+                    <TextField fullWidth label='Name' placeholder="Enter your name" />
+                    <TextField fullWidth label='Email' placeholder="Enter your email" />
+                    <FormControl component="fieldset" style={marginTop}>
+                        <FormLabel component="legend">Gender</FormLabel>
+                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
+                            <FormControlLabel value="female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        </RadioGroup>
+                    </FormControl>
+                    <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
+                    <TextField fullWidth label='Password' placeholder="Enter your password"/>
+                    <TextField fullWidth label='Confirm Password' placeholder="Confirm your password"/>
+                    <FormControlLabel
+                        control={<Checkbox name="checkedA" />}
+                        label="I accept the terms and conditions."
+                    />
+                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
+                </form>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Signup;
---
+++ src/components/Tables/TollDetails.js
@@ -0,0 +1,77 @@
+
+import React from 'react'
+import MaterialTable from 'material-table'
+
+const TollDetails = () => {
+    const { useState } = React;
+    const [selectedRow, setSelectedRow] = useState(null);
+  
+    const [columns, setColumns] = useState([
+      { title: 'District', field: 'district' },
+      { title: 'Toll Name', field: 'tollname', initialEditValue: 'initial edit value' },
+      { title: 'Road Section', field: 'roadsection' },
+    
+    ]);
+  
+    const [data, setData] = useState([
+      { district: 'Lilongwe', tollname: 'Area 25', roadsection: 'Bt - Lilongwe' },
+      { district: 'Mzimba', tollname: 'Luviri', roadsection: 'Jenda - Mzimba Boma'},
+      { district: 'Blantyre', tollname: 'Kameza', roadsection: 'Blantyre Town - Lunzu' },
+      { district: 'Zomba', tollname: '3 Miles', roadsection: 'Limbe - Zomba'},
+    ]);
+  
+    return (
+      <MaterialTable
+        title="Toll Details"
+        columns={columns}
+        data={data}
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                setData([...data, newData]);
+                
+                resolve();
+              }, 1000)
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.id;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+  
+                resolve();
+              }, 1000)
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                
+                resolve()
+              }, 1000)
+            }),
+        }}
+
+        onRowClick={((evt, selectedRow) => setSelectedRow(selectedRow.tableData.id))}
+        options={{
+          rowStyle: rowData => ({
+            backgroundColor: (selectedRow === rowData.tableData.id) ? '#EEE' : '#FFF'
+        })
+      }}
+
+      options = {{
+          exportButton : true
+      }}
+
+      
+      />
+    )
+  }
+
+  export default TollDetails

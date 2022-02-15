6bea5f6d1dff4caecef18be1235d2054025bc4ce
SolomonSatoh <solohsatoh0@gmail.com>
2022-02-15 05:15:53 +0200

    merged tolldetails with the api data

--- src/components/Tables/TollDetails.js
+++ src/components/Tables/TollDetails.js
@@ -1,25 +1,40 @@
 
-import React from 'react'
-import MaterialTable from 'material-table'
+import React from 'react';
+import MaterialTable from 'material-table';
+import  {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const TollDetails = () => {
-    const { useState } = React;
+    // const { useState } = React;
+    const [data, setData] = useState([]);
     const [selectedRow, setSelectedRow] = useState(null);
   
     const [columns, setColumns] = useState([
       { title: 'District', field: 'district' },
-      { title: 'Toll Name', field: 'tollname', initialEditValue: 'initial edit value' },
-      { title: 'Road Section', field: 'roadsection' },
+      { title: 'Toll Name', field: 'tollName', initialEditValue: 'initial edit value' },
+      { title: 'Road Section', field: 'section' },
     
     ]);
-  
-    const [data, setData] = useState([
-      { district: 'Lilongwe', tollname: 'Area 25', roadsection: 'Bt - Lilongwe' },
-      { district: 'Mzimba', tollname: 'Luviri', roadsection: 'Jenda - Mzimba Boma'},
-      { district: 'Blantyre', tollname: 'Kameza', roadsection: 'Blantyre Town - Lunzu' },
-      { district: 'Zomba', tollname: '3 Miles', roadsection: 'Limbe - Zomba'},
-    ]);
-  
+   
+    // const [data, setData] = useState([
+    //   { district: 'Lilongwe', tollname: 'Area 25', roadsection: 'Bt - Lilongwe' },
+    //   { district: 'Mzimba', tollname: 'Luviri', roadsection: 'Jenda - Mzimba Boma'},
+    //   { district: 'Blantyre' , tollname: 'Kameza', roadsection: 'Blantyre Town - Lunzu' },
+    //   { district: 'Zomba', tollname: '3 Miles', roadsection: 'Limbe - Zomba'},
+    // ]);
+    const baseUrl="http://localhost:3001/tolls/";
+
+    const getTolls=async()=>{
+      await axios.get(baseUrl)
+      .then(Response=>{
+        setData(Response.data);
+      })
+    }
+     
+    useEffect(()=>{
+      getTolls();
+    }, [])
+
     return (
       <MaterialTable
         title="Toll Details"

b5c260b75234d2600e37714bc11b52c283adedac
MARTINCHUNGA <BSC/77/16@COMLAB.COM>
2022-03-15 02:10:52 +0200

    implemented stats graph with api data

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -1,64 +1,124 @@
 
-import React from 'react';
-import { Bar, defaults } from 'react-chartjs-2';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import {Bar} from 'react-chartjs-2'
 
-defaults.global.tooltips.enabled = false;
-defaults.global.legend.position = 'bottom';
+const DynamicChart = () => {
+    const [chartData, setChartData]  = useState({});
+    
+ const Chart = () => {
 
-const BarChart = () => {
-  return (
-    <div>
-      <Bar
-        data={{
-          labels: ['Bus', 'Trucks', 'Luxury Cars', 'Minibus', 'Abnormal Truck', 'Motor Cycle'],
-          datasets: [
-            {
-              label: 'Vehicle Categories',
-              data: [20000, 15000, 30000, 100000, 10000, 200000],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-             
-              borderWidth: 1,
-            },
-           
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-          legend: {
-            labels: {
-              fontSize: 25,
-            },
-          },
-        }}
-      />
-    </div>
-  );
+    axios.get("http://localhost:3001/bookings")
+    .then(res => {
+   
+       console.log('Incoming data', res.data)
+        var startArray = res.data, finalArray = [];
+
+        startArray.forEach(function(e){
+            if(!this[e.vehicleType]){
+              this[e.vehicleType]  = {vehicleType: e.vehicleType, price: 0}
+              finalArray.push(this[e.vehicleType]);
+            }
+            this[e.vehicleType].price += Number(e.price)
+        }, {})
+
+        //get vehicles and prices into separate arrays
+        let vehicles = finalArray.map(vehicleCategory => vehicleCategory.vehicleType);
+        let prices = finalArray.map(vehiclePrice => vehiclePrice.price)
+
+
+       
+        
+            setChartData({
+                labels: vehicles,
+                datasets: [{
+                                             label: 'price per car category',
+                                             data: prices,
+                                             backgroundColor: [
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                             ],
+                                             borderColor: [
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                             ],
+                                             borderWidth: 1
+                                         }]
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+        
+    }
+    useEffect(() => {
+        Chart();
+      }, []);
+      return(
+          <div className="App">
+              <h1>Bar Chart</h1>
+              <div>
+                  <Bar
+                    data={chartData}
+                    options={{
+                        responsive:true,
+                        title: { text: "GRAPH SHOWING PRICE AGAINST VEHICLE CATEGORIES", display: true },
+                        scales:{
+                            yAxes:{
+                                ticks:{
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    }}
+                  />
+              </div>
+          </div>
+      )
 }
 
-export default BarChart;
+export default DynamicChart;

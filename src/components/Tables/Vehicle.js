b4601c0b8d13b080ab84b3b6b96f30339e3f1b40
MARTINCHUNGA <Bsc-77-16@cc.ac.mw>
2022-04-24 11:21:28 -0700

    vehicle changes

--- src/components/Tables/Vehicle.js
+++ src/components/Tables/Vehicle.js
@@ -22,7 +22,7 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
-import Alert from '@material-ui/lab/Alert';
+//import Alert from '@material-ui/lab/Alert';
 
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
